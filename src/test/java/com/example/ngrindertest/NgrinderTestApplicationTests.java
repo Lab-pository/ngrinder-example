af6bba241a2de0c3838e93ca869c6aef8f979287
ProtoSeo <proto_seo@naver.com>
2023-08-08 14:34:34 +0900

    Ngrinder test 프로젝트 초기화

---
+++ src/test/java/com/example/ngrindertest/NgrinderTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ngrindertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgrinderTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
